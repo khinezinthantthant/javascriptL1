794dd210d29acf2d62212fafdbc4f43e188bc681
khinezinthantthant <khinezinthant.dev@gmail.com>
2022-09-21 10:57:40 +0630

    21 Sept 2022

---
+++ l2scrollprogress/app.js
@@ -0,0 +1,41 @@
+var getprogressbar = document.getElementById("progress-bar");
+
+window.onscroll = function(){
+	scrollpoint();
+};
+
+function scrollpoint(){
+	// console.log('i am working');
+
+
+	// scrolltop
+	// project height
+	// current height
+	// project height - current height
+	// scrolltop * 100 / (project height - current height)
+
+
+	var getscrolltop = document.documentElement.scrollTop;
+	// console.log(getscrolltop);
+	var getscrollheight = document.documentElement.scrollHeight;
+	// console.log(getscrollheight);
+	var getclientheight = document.documentElement.clientHeight;
+	// console.log(getclientheight);
+
+	var calheight = getscrollheight - getclientheight;
+
+	var getfinal =Math.round((getscrolltop * 100) / calheight);
+	// console.log(getfinal);
+
+	var getfinal = Math.round((getscrolltop/calheight) * 100);
+	// console.log(getfinal);
+
+	getprogressbar.style.width=`${getfinal}%`;
+
+	
+}
+
+
+function printme(){
+	window.print();
+}
---
+++ l3progressbar/app.js
@@ -0,0 +1,25 @@
+var getprogressbar = document.querySelector(".progress-bar");
+var getdownloadbtn = document.querySelector(".download-btn");
+var seturl ="https://linkedin.com";
+
+getdownloadbtn.addEventListener("click",function(){
+	// console.log('hi');
+
+	getdownloadbtn.setAttribute("disabled",true);
+
+	var setwidth = 0;
+
+	var setintv = setInterval(progressinc,100);
+
+	function progressinc(){
+		if(setwidth >= 100){
+			setwidth = 0;
+			clearInterval(setintv);
+		}else{
+			setwidth++;
+
+			getprogressbar.style.width = `${setwidth}%`;
+			getprogressbar.setAttribute("data-inc",`${setwidth}%`);
+		}
+	}
+});
